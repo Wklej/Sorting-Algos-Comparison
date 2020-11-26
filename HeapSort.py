f835242946d82b3318ecdac7c29d58e51ba2f656
Wklej <59125969+Wklej@users.noreply.github.com>
2020-11-26 21:28:27 +0100

    update

--- HeapSort.py
+++ HeapSort.py
@@ -1,6 +1,3 @@
-from random import random, randrange
-
-
 def heapify(arr, size, index):
     largest = index
     left = 2 * index + 1
@@ -26,9 +23,3 @@ def heapSort(arr):
     for i in range(size - 1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]
         heapify(arr, i, 0)
-
-
-# array = [randrange(10000) for _ in range(1000)]
-# print(array)
-# heapSort(array)
-# print(array)
